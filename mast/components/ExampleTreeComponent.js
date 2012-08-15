ce231bd59ba7fd5bf1251e662896e4cacd9c7946
Mike McNeil <michael.r.mcneil@gmail.com>
2012-08-15 02:38:33 -0500

    Added CSS to make the tree sample easier to see.  Also exercised the 'contextmenu' event support (right-click).

--- mast/components/ExampleTreeComponent.js
+++ mast/components/ExampleTreeComponent.js
@@ -1,23 +1,42 @@
 Mast.components.ExampleTreeComponent = Mast.Tree.extend({
 	
+	events: {
+		'click a.doDelete':'deleteTree' ,
+		'click a.addButton': 'addTree',
+		'contextmenu':	'_test'
+	},
 	outlet: '.sandbox',
 	model: 'ExampleTreeModel',
 	template: '#mast-template-exampleTree',
+	emptyHTML: '<em>There are no children available.</em>',
 	
 	branchOutlet: '.branchOutlet',
 	branchComponent: "ExampleTreeComponent",
 	collection: "ExampleTreeCollection",
-	
-	events: {},
+
 	
 	// Called after initialization, before autorender
-	init: function () {
+	init: function () {},
+	
+	afterRender: function () {
+		this.$el.disableSelection();
 	},
 	
 	changeName: function(formFieldValue) {
 		this.set('name',formFieldValue);
 	},
-	afterRender: function () {
-		this.$el.disableSelection();
+	
+	deleteTree: function (e) {
+		// Stop event from propagating up to parent components
+		e.stopImmediatePropagation();
+	
+		this.destroy();
+	},
+	
+	addTree: function (e) {
+		// Stop event from propagating up to parent components
+		e.stopImmediatePropagation();
+		
+		this.collection.add({value: this.get('value')+1});
 	}
 });
