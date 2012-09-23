a6368f481d892ca8a30eb0cc94df4e9e9a913e54
Mike McNeil <michael.r.mcneil@gmail.com>
2012-09-22 23:57:53 -0500

    Removed extraneous class method from Experiment model (now all of that is taken care of in the sails scaffold.

--- models/Experiment.js
+++ models/Experiment.js
@@ -1,27 +1,5 @@
 Experiment = Model.extend({
-	
 	title: STRING,
 	value: INTEGER,
-	highlighted: BOOLEAN,
-	
-	classMethods: {
-		
-		fetch: function(options,callback) {
-			
-			var self = this;
-			Experiment.findAll(
-			{
-				order: 'title ASC',
-				limit: 25,
-				offset: 0
-			}).success(function(objs){
-				if (objs) {
-					callback(objs);
-				}
-				else {
-					throw new Error ();
-				}
-			})
-		}
-	}
+	highlighted: BOOLEAN
 });
