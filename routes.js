38bdd97771225c5df45e6bc8078ed8d4c66fb289
Mike McNeil <michael.r.mcneil@gmail.com>
2012-10-14 13:16:03 -0500

    Cleanup to use new config files.

---
+++ routes.js
@@ -0,0 +1,47 @@
+// Custom URL mappings
+// *********************
+// 
+// Use for situations where:
+//		/controllerName/methodName
+// and backbone semantics (routing by HTTP verb) are not enough
+// 
+// You can override default url mappings (404,500,home) here as well.
+//
+module.exports = {
+
+	// Default mappings
+	'/': {
+		controller: 'meta',
+		action: 'home'
+	},
+	'/500': {
+		controller: 'meta',
+		action: 'error'
+	},
+	'/404': {
+		controller: 'meta',
+		action: 'notfound'
+	},
+	'/403': {
+		controller: 'meta',
+		action: 'denied'
+	},
+
+	// Authentication mappings
+	'/login': {
+		controller: 'auth',
+		action: 'login'
+	},
+	'/logout': {
+		controller: 'auth',
+		action: 'logout'
+	},
+	'/register': {
+		controller: 'auth',
+		action: 'register'
+	},
+	'/tj': {
+		controller: 'settings',
+		action: 'testjson'
+	}
+};
